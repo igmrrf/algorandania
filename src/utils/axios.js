fb854031d42796ebf462f6482d7fb1fcb4c7c971
igmrrf <francis.igbiriki.fi@gmail.com>
2020-10-02 03:46:23 +0100

    Axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { token } from "./constants";
 let url;
 if (process.env.NODE_ENV !== "production") url = "http://localhost:8080/api/";
 else url = "https://bcmt.herokuapp.com/api/";
@@ -8,7 +7,6 @@ console.log(url);
 export default axios.create({
   baseURL: url,
   headers: {
-    "x-auth-token":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZjcxM2EyOGY0YzNlYTAwMTcyMmQ5NWMiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MDE1OTExMjN9.KvgShSQWQJ1DBEbja746PBMyTzENavHiLv4rLORC3hc",
+    "x-auth-token": window.localStorage.getItem("x-auth-token"),
   },
 });
